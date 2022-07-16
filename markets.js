9a8740f26b2afd395a2341f20a6939bf2468d8e7
CoderMatthiasGSM <87883303+CoderMatthiasGSM@users.noreply.github.com>
2022-07-16 11:10:12 -0400

    Updated - Credit to DSG

--- markets.js
+++ markets.js
@@ -1,96 +1,146 @@
 const markets = {
-    // OpenSea Contract Address
-    '0x7f268357a8c2552623316e2562d90e642bb538e5' : {
-        'name': 'OpenSea',
-        'site': 'https://opensea.io/assets/',
-        'logDecoder': [{
-            type: 'bytes32',
-            name: 'buyHash'
-        }, {
-            type: 'bytes32',
-            name: 'sellHash',
-        },{
-            type: 'uint256',
-            name: 'price',
-        }]
-    },
-
-// OpenSea Seaport Contract Address
-  "0x00000000006c3852cbef3e08e8df289169ede581": {
-    name: "OpenSea 🌊 Seaport",
-    site: "https://opensea.io/assets/",
+  // X2Y2
+  '0x74312363e45dcaba76c59ec49a7aa8a65a67eed3': {
+    name: 'X2Y2 ⭕️',
+    site: 'https://x2y2.io/eth/',
     logDecoder: [
       {
-        name: "orderHash",
-        type: "bytes32",
+        type: 'bytes32',
+        name: 'itemHash',
       },
       {
-        name: "purchaser",
-        type: "address",
+        type: 'address',
+        name: 'currency',
       },
       {
-       
-        name: "offerOffset",
-        type: "uint256",
+        type: 'address',
+        name: 'to',
       },
       {
-        name: "considerationOffset",
-        type: "uint256",
-      },{
-        name: "offerLength",
-        type: "uint256",
-      },
-      {
-        name: "offerItemType",
-        type: "uint256",
-      },
-      {
-        name: "offerToken",
-        type: "address",
-      },
-      {
-        name: "offerIdentifier",
-        type: "uint256",
-      },
-      {
-        name: "offerAmount",
-        type: "uint256",
-      },
-      {
-        name: "offerConsiderationLength",
-        type: "uint256",
+        type: 'uint256',
+        name: 'amount',
       },
     ],
   },
-    // LooksRareExchange Contract Address
-    '0x59728544b08ab483533076417fbbb2fd0b17ce3a' : {
-        'name': 'LooksRare 👀💎',
-        'site': 'https://looksrare.org/collections/',
-        'logDecoder': [{
-            type: 'bytes32',
-            name: 'orderHash'
-        }, {
-            type: 'uint256',
-            name: 'orderNonce',
-        },{
+  // OpenSea Wyvern
+  '0x7f268357a8c2552623316e2562d90e642bb538e5': {
+    name: 'OpenSea 🌊',
+    site: 'https://opensea.io/assets/',
+    logDecoder: [
+      {
+        type: 'bytes32',
+        name: 'buyHash',
+      },
+      {
+        type: 'bytes32',
+        name: 'sellHash',
+      },
+      {
+        type: 'uint256',
+        name: 'price',
+      },
+    ],
+  },
+  // LooksRareExchange
+  '0x59728544b08ab483533076417fbbb2fd0b17ce3a': {
+    name: 'LooksRare 👀💎',
+    site: 'https://looksrare.org/collections/',
+    logDecoder: [
+      {
+        type: 'bytes32',
+        name: 'orderHash',
+      },
+      {
+        type: 'uint256',
+        name: 'orderNonce',
+      },
+      {
+        type: 'address',
+        name: 'currency',
+      },
+      {
+        type: 'address',
+        name: 'collection',
+      },
+      {
+        type: 'uint256',
+        name: 'tokenId',
+      },
+      {
+        type: 'uint256',
+        name: 'amount',
+      },
+      {
+        type: 'uint256',
+        name: 'price',
+      },
+    ],
+  },
+  // Opensea - Seaport
+  '0x00000000006c3852cbef3e08e8df289169ede581': {
+    name: 'Opensea ⚓️',
+    site: 'https://opensea.io/assets/',
+    logDecoder: [
+      {
+        type: 'bytes32',
+        name: 'orderHash',
+      },
+      {
+        type: 'address',
+        name: 'recipient',
+      },
+      {
+        type: 'tuple[]',
+        name: 'offer',
+        components: [
+          {
+            type: 'uint8',
+            name: 'itemType',
+          },
+          {
             type: 'address',
-            name: 'currency',
-        },{
-            type: 'address',
-            name: 'collection',
-        },{
+            name: 'token',
+          },
+          {
             type: 'uint256',
-            name: 'tokenId',
-        },{
+            name: 'identifier',
+          },
+          {
             type: 'uint256',
             name: 'amount',
-        },{
+          },
+        ],
+      },
+      {
+        type: 'tuple[]',
+        name: 'consideration',
+        components: [
+          {
+            type: 'uint8',
+            name: 'itemType',
+          },
+          {
+            type: 'address',
+            name: 'token',
+          },
+          {
             type: 'uint256',
-            name: 'price',
-        }]
-    }
+            name: 'identifier',
+          },
+          {
+            type: 'uint256',
+            name: 'amount',
+          },
+          {
+            type: 'address',
+            name: 'recipient',
+          },
+        ],
+      },
+    ],
+  },
 };
 
 module.exports = {
-    markets: markets
+  markets: markets,
 };
